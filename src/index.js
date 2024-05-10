6bfa24804b1d6210395a1d39d6350c1249cc8011
Ian Pares <yannpares@gmail.com>
2024-05-10 11:43:15 +0200

    first item loaded

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import express from "express"
+const express = require('express')
+const axios = require('axios');
+require('dotenv').config()
+
 
 const app = express()
+const db = process.env.DB_CONNECTION
+const PORT = process.env.PORT || 3000;
+const n = 1
+const itemsPerPage = 30;
 
-app.get('/', (req, res) => res.send("this works"))
 
-app.listen(3000)
-console.log("server is listening on port", 3000)
+
+app.get(`/`, async (req, res) => {
+    try {
+        const response = await axios.get(`${db}/${1}.json?print=pretty`, {
+        })
+        res.json(response.data)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+
+
+
+    // remember ---> 
+
+
+    app.listen(PORT)
+    console.log("server is listening on port", PORT)
