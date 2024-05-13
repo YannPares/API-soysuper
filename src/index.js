1d43566a8beba847b0ae322721f6fe1ef2ce60d3
Ian Pares <yannpares@gmail.com>
2024-05-13 15:53:14 +0200

    (fixed) problem with cache

--- src/index.js
+++ src/index.js
@@ -6,24 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 let cache = {}
 
-let isEmpty = false;
-
 app.get(['/:page','/', '*'], async (req, res) => {
     
     let page = req.params.page || 1
     console.log(page)
 
-    for (let i = 1; i <= page && !isEmpty; i++ ){
+    for (let i = 1; i <= page; i++ ){
         if (!cache[i]){
             const data = await scraping(i);
             cache[i] = data;
             console.log(i)  
-        } 
+        }
     }
     const data = cache[page]
     console.log(data)
     const dataFormat = JSON.stringify(data, null, 2);
-    res.setHeader('Content-Type', 'application/json');  
+    res.setHeader('Content-Type', 'application/json');    
     res.send(dataFormat); 
      
 });
