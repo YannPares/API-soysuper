644b048f9f8d9d9f6f4a307645abce83052cdb24
Ian Pares <yannpares@gmail.com>
2024-05-13 13:01:57 +0200

    (add) redirect wrong path

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ const { scraping } = require('./scrap.js');
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.get(['/:page','/'], async (req, res) => {
+app.get(['/:page','/', '*'], async (req, res) => {
     
     let page = req.params.page
     console.log(page)
-    
     const data = await scraping(page);
     const dataFormat = JSON.stringify(data, null, 2);
     res.setHeader('Content-Type', 'application/json'); 
