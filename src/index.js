ab2ebddca40e283d348fc678dcea9c52f199e5d6
Ian Pares <yannpares@gmail.com>
2024-05-13 10:48:03 +0200

    (fix) export function to import on server

--- src/index.js
+++ src/index.js
@@ -1,41 +1,15 @@
-// const express = require('express')
-// const axios = require('axios');
-// require('dotenv').config()
+import express from 'express'
+import { scraping } from './scrap.mjs';
 
+const app = express()
+const PORT = process.env.PORT || 3000;
 
-
-// const app = express()
-// const db = process.env.DB_CONNECTION
-// const PORT = process.env.PORT || 3000;
-// const n = 1
-// const itemsPerPage = 30;
-
-
-
-
-
-
-
-
-
-// app.get(`/`, async (req, res) => {
-//     res.send('hello')
-//     // try {
+app.get('/', async (req, res) => {
+    const data = await scraping();
+ 
+    res.json(data);
         
-//     //     const response = await axios.get(`${db}/${1}.json?print=pretty`, {
-//     //     })
-//     //     res.json(response.data)
-    
-//     // } catch (err) {
-//     //     res.status(500).json({ message: err.message })
-//     // }
-// })
+ });
 
-
-
-
-//     // remember ---> 
-
-
-//     app.listen(PORT)
-//     console.log("server is listening on port", PORT)
+app.listen(PORT)
+console.log("server is listening on port", PORT)
--- src/scrap.mjs
+++ src/scrap.mjs
@@ -1,32 +1,31 @@
 import { chromium } from "playwright";
 
-(async () => {
+
+export async function scraping () {
     const browser = await chromium.launch({ headless: true });
     const page = await browser.newPage();
 
     await page.goto('https://news.ycombinator.com/');
 
-   
-
-    const elements = await page.$$eval('tr.athing', (results) => (
+    const elements = await page.$$eval('tr.athing:nth-of-type(-n+90)', (results) => (
     results.map((el) => {
 
         const title = el.querySelector('.title a')?.innerText;
         if (!title) return null;
-
         const points = el.nextElementSibling.querySelector('.score')?.innerText;
-
         const send_by = el.nextElementSibling.querySelector('.hnuser')?.innerText;
-
         const published = el.nextElementSibling.querySelector('.age')?.innerText;
-
-        const comments = el.nextElementSibling.querySelector('.subtext').innerText;
-        
+        const comments = el.nextElementSibling.querySelector('.subtext');
 
         return { title, points, send_by, published, comments };
     })
-));
-
-    console.log(elements);
+)); 
     await browser.close();
-})();
+    return elements;
+   
+    
+};
+
+
+
+
