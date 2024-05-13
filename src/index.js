ef2ceb637e3a837b155bc7f8afb06dbcd6e5181d
Ian Pares <yannpares@gmail.com>
2024-05-13 12:59:34 +0200

    (solved) loading first page with no path

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const { scraping } = require('./scrap.js');
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.get(`/:page`, async (req, res) => {
-    const page = req.params.page
+app.get(['/:page','/'], async (req, res) => {
+    
+    let page = req.params.page
     console.log(page)
-
+    
     const data = await scraping(page);
     const dataFormat = JSON.stringify(data, null, 2);
     res.setHeader('Content-Type', 'application/json'); 
