726697c8c425aae1fcd8f231692c66438395552f
Ian Pares <yannpares@gmail.com>
2024-05-13 10:59:28 +0200

    (fix) Json format on server

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
     const data = await scraping();
- 
-    res.json(data);
-        
- });
+    const dataFormat = JSON.stringify(data, null, 2)
+    res.send(dataFormat)
+});
 
 app.listen(PORT)
 console.log("server is listening on port", PORT)
