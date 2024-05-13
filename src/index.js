17695397d56bd0164a4fd5945afd98cbd3e4cb40
Ian Pares <yannpares@gmail.com>
2024-05-13 15:49:43 +0200

    (add) CACHE. Is working, but only shows one page. Pending solve sometimes 'sorry.' message

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ const { scraping } = require('./scrap.js');
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+let cache = {}
+
+let isEmpty = false;
+
 app.get(['/:page','/', '*'], async (req, res) => {
     
-    let page = req.params.page
+    let page = req.params.page || 1
     console.log(page)
-    const data = await scraping(page);
+
+    for (let i = 1; i <= page && !isEmpty; i++ ){
+        if (!cache[i]){
+            const data = await scraping(i);
+            cache[i] = data;
+            console.log(i)  
+        } 
+    }
+    const data = cache[page]
+    console.log(data)
     const dataFormat = JSON.stringify(data, null, 2);
-    res.setHeader('Content-Type', 'application/json'); 
+    res.setHeader('Content-Type', 'application/json');  
     res.send(dataFormat); 
+     
 });
 
 app.listen(PORT)
